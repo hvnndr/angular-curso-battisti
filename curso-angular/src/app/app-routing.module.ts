046338a88eb6d3a62e75a0d2674fb4347e1724b1
Hevannder Rubian <hevannder.albuquerque@fpf.br>
2022-06-19 21:32:39 -0400

    lesson 17 now ready to 18

--- curso-angular/src/app/app-routing.module.ts
+++ curso-angular/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FirstComponentComponent} from "./components/first-component/first-component.component";
+import {ListRenderComponent} from "./components/list-render/list-render.component";
+import {IfRenderComponent} from "./components/if-render/if-render.component";
+import {EventsComponent} from "./components/events/events.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent},
+  {path:'list', component: ListRenderComponent},
+  {path: 'if', component: IfRenderComponent},
+  {path: 'event', component: EventsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
